ca00560131809aa304ec8e1da6aa4f072ba23df0
Alexander Scheurer <mail@aspepex.net>
2012-01-08 22:27:08 +0100

    Eclipse setup and first test

---
+++ src/FuType.java
@@ -0,0 +1,20 @@
+import processing.core.*;
+
+public class FuType extends PApplet {
+
+	public void setup() {
+		size(100,200);
+	}
+	
+	public void draw() {
+		ellipse(10,10,10,10);
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main(new String[] { "--bgcolor=#ECE9D8", "Fu-Type" });
+	}
+
+}
