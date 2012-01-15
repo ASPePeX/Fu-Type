cc4e68d9fd6d5df5d4e689c863ce6f0df3514b29
Alexander Scheurer <mail@aspepex.net>
2012-01-15 22:23:48 +0100

    Some basic stuff

--- src/FuType.java
+++ src/FuType.java
@@ -3,12 +3,33 @@ import processing.core.*;
 public class FuType extends PApplet {
 
 	public void setup() {
-		size(100,200);
+		size(800, 600);
+		Ship s1 = new Ship(100, 100);
+		Ship.p = this;
+		Weapon w[] = new Weapon[200];
+		
 	}
-	
+
 	public void draw() {
-		ellipse(10,10,10,10);
+		background(0);
+		Ship.draw();
+
+	if (keyPressed) {
+			if (key == 's') {
+				Ship.moveDown();
+			}
+			else if (key == 'w') {
+				Ship.moveUp();
+			}
+			else if (key == 'a') {
+				Ship.moveLeft();
+			}
+			else if (key == 'd') {
+				Ship.moveRight();
+			}
+		}
 	}
+
 	/**
 	 * @param args
 	 */
