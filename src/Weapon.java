34b8a69349aa6e6d2ed4c1850e09fccf7a5a094f
Alexander Scheurer <mail@aspepex.net>
2012-01-15 22:24:23 +0100

    Not much

---
+++ src/Weapon.java
@@ -0,0 +1,19 @@
+import processing.core.PApplet;
+
+public class Weapon {
+	int x, y;
+	// player is set when the shot originates from the player. true => shot
+	// direction left to right, damages Enemies, doesn't damage player. false =>
+	// the other way around.
+	boolean player;
+	boolean active; 
+	
+	static PApplet p;
+
+	Weapon() {
+		
+	}
+	public void shootLaser1() {
+		
+	}
+}
